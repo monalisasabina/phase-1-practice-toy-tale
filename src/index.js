66b56a987f536b0a872cc838f5508defd5459f16
Monalisa Onyango <+>
2024-07-14 20:54:41 +0300

    check llater

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
   const addBtn = document.querySelector("#new-toy-btn");
+
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -12,4 +14,85 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  function fetchToys(){
+
+    fetch('http://localhost:3000/toys')
+     .then((response) => response.json())
+    .then(toys => toys.forEach(toy => renderOneToy()))
+  }
+
+  function initialize (){
+   fetchToys()
+  }
+  initialize();
+
+  function renderOneToy(toy){
+  
+    let card=document.createElement('li')
+    card.innerHTML=`
+       <div class="card">
+         <h2>${toy.name}</h2>
+         <img> src= "${toy.image}" class="toyAvatar" >
+         <p> ${toy.likes} <strong>likes</strong></p>
+         <button class="like-btn" id="[toy_id]">Like ❤️</button>
+       </div>
+    `
+
+    // PATCH
+    card.document.querySelector("button#like-btn").addEventListener('click', () => {
+     toy.likes +=1
+     updateTOY(toy)
+     })
+    
+  }
+
+ 
+
+  // ...................................................................
+  // POST
+
+  document.querySelectorAll('form#add-toy-form'),addEventListener('submit',handleSubmit)
+      
+     function handleSubmit(event){
+        event.preventDefault();
+
+        let toyObject ={
+          name: event.target.name.value,
+          image: event.target.image.value,
+          likes: 5
+        }
+       renderOneToy(toyObject) 
+       addToy(toyObject)
+     
+    
+     function addToy(){
+     fetch('http://localhost:3000/toys',{
+     method: 'POST',
+     headers: {
+      'Content-Type':'application/json'
+      },
+     body:JSON.stringify(toyObject)  
+    })
+    .then((response) => response.json())
+    .then( toy => console.log(toy))
+  }
+
+
+//......................................................................... 
+// PATCH: INCREASING TOY LIKES 
+  
+ function updateTOY(){
+  fetch('http://localhost:3000/toys',{
+    method: 'PATCH',
+    headers: {
+     'Content-Type':'application/json'
+     },
+    body:JSON.stringify(toyObject)  
+   })
+   .then((response) => response.json())
+   .then( toy => console.log(toy))
+ }
+ 
+     }
 });
